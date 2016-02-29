11eb0423044bd10de58028c0da6b75d8aa91efb8
jiapengfei <1144670005@qq.com>
2016-02-29 23:35:37 +0800

    23

--- test/com/jpf/test/Test.java
+++ test/com/jpf/test/Test.java
@@ -4,6 +4,6 @@ package com.jpf.test;
 public class Test{
 
 	public static void main(String[] args){
-		System.out.println(TestMD5.MD5Encode("12345678"));
+		System.out.println(TestMD5.MD5Encode("123456"));
 	}
 }
